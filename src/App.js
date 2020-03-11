0a294c15c5a3d2949a657602a831b22dfb703b19
Jude Agboola <marvinjudehk@gmail.com>
2020-03-11 15:20:28 +0100

    fix padding

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Router>
         <Navigation />
         <Switch>
-          <section className="flex flex-wrap px-2 sm:px-20 m-3">
+          <section className="flex flex-wrap px-2 md:px-20 m-3">
             <Route exact path="/">
               <Redirect to={{ pathname: "/chart" }} />
             </Route>
